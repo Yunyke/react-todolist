0fed73cc6f8cfa960cf88bc7a392ff1ac32ae827
Yunyke <min221218@gmail.com>
2025-07-15 22:47:56 +0800

    aop practice

--- src/main/java/com/example/demo/aspect/ExecutionTimeAspect.java
+++ src/main/java/com/example/demo/aspect/ExecutionTimeAspect.java
@@ -8,6 +8,15 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class ExecutionTimeAspect {
+	
+	/*
+	 * AOP（面向切面編程）是一種將非核心但經常重複出現的業務邏輯，從核心業務代碼中分離出來的設計方式。
+	 * 這些重複邏輯會被獨立建立為所謂的「切面」（Aspect），
+	 * 例如日誌記錄（logging）、權限驗證（authorization）、例外處理、事務管理等。
+	 * 這些功能雖然不直接影響業務邏輯本身，卻廣泛存在於多個業務流程中。透過 AOP，可以將這些「橫切關注點」統一管理，
+	 * 提升系統的可維護性與可讀性，並減少重複程式碼。
+	 */
+
 
 	
 	@Around("execution(* com.example.demo.controller..*(..))")
