af92952d7735c47a3db0e92537a627116c3d5b03
Yunyke <min221218@gmail.com>
2025-08-04 23:31:46 +0800

    t

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("react/todoss")
+@RequestMapping("react/todos")
 @CrossOrigin(origins = "http://localhost:5173")
 public class TodoController {
 
